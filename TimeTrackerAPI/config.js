160d1bf4e876a0f51e3b92efd67dd2fffc7420ca
raju05528 <vasantha.raju@msisoft.in>
2017-07-31 11:12:53 +0530

    no message

---
+++ TimeTrackerAPI/config.js
@@ -0,0 +1,22 @@
+var config = {};
+config.jwt = {};
+
+config.jwt.secret = '1CRiBb6AAjQxrd/DPYlQQ+Lg/5/l4TVPE9XIwHvE/2U=';
+config.cj360URL = 'https://collect.cj.bluedme.com/eshop.renault/v1';
+config.activestatus = '1';
+config.TwoNumber = '2';
+config.OneNumber = '1';
+config.InActivestatus = '0';
+config.eventschdulerHour = '14';
+config.eventschdulerminute = '53';
+config.eventschdulersecond = '0';
+config.prospectschdulerHour = '23'
+config.prospectschdulerminute='50';
+config.prospectschdulersecond= '0';
+config.prospectInactiveschdulerHour= '1';
+config.prospectInactiveschdulerminute='0';
+config.prospectInactiveschdulersecond='0';
+config.authenticationIssuerTag = 'cloudium';
+config.Event_Type_Genius_Login = 'genius_login';
+config.Include_CarConfiguration_Event = 'Configurator';
+module.exports = config;
